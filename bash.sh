75aa0a5d1d49a7a19f4d1a13684a3e2109d59ef3
retr0717 <alwinaji717@gmail.com>
2024-07-07 17:46:25 +0530

    printing in terminal using printf

--- bash.sh
+++ bash.sh
@@ -13,3 +13,8 @@ printf "hello"
 
 echo hello
 hello
+
+printf "%-5s %-10s %-4s\n" No Name Mark
+printf "%-5s %10s %-4.2f\n" 1 Sarath 80.3456 # a length of 10 spaces are allowed to print the string from the left.
+printf "%-5s %-10s %-4.2f\n" 2 James 90.99891
+printf "%-5s %-10s %-4.2f\n" 3 Jeff 77.564
